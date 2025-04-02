70ff64ea929ecaea897323566b3f68145f7f83b2
Noah Blair <43483259+sodium-hydroxide@users.noreply.github.com>
2025-04-02 14:50:05 -0600

    Add changes to comments for bash aliases

--- .config/bash/alias.sh
+++ .config/bash/alias.sh
@@ -18,17 +18,17 @@ cd_ls() {
     ls
 }
 
-alias lsold="ls"       # Use the typical ls command
-alias ls="ls -CFl"     # List Files in a List
-alias lsa="ls -CFla"   # List All Files in a List
-alias vim="vi"         # Open VIm
-alias lfold="lf"       # List Files (without Changing Directory)
-alias lf="lf_exit"     # List Files (and Change Directory)
-alias kd="cd_ls"       # Change Directory and List Contents
+alias lsold="ls"     # Use the typical ls command
+alias ls="ls -CFl"   # List Files in a List
+alias lsa="ls -CFla" # List All Files in a List
+alias vim="vi"       # Open VIm
+alias lfold="lf"     # List Files (without Changing Directory)
+alias lf="lf_exit"   # List Files (and Change Directory)
+alias kd="cd_ls"     # Change Directory and List Contents
 alias icat="kitten icat"
 alias cluster="ssh njblair@129.82.20.78"
 alias web="awrit https://html.duckduckgo.com/html/"
 alias rstudio="open -a RStudio"
 alias dotedit="vi ${HOME}/dotfiles"
 alias colostate="cd ${HOME}/Files/COLOSTATE && ls"
-
+alias pythonista="code '/Users/noa3hjb/Library/Mobile Documents/iCloud~com~omz-software~Pythonista3/Documents'"
