a0e7b05ff8b6f6b81984be3ab6f1e33f13e61fcd
Noah Blair <43483259+sodium-hydroxide@users.noreply.github.com>
2025-04-21 11:32:22 -0600

    Clean up toggle term configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -299,25 +299,7 @@ require("lazy").setup({
   {
     "akinsho/toggleterm.nvim",
     version = "*",
-    opts = {
-      size = function(term)
-        if term.direction == "horizontal" then
-          return math.floor(vim.o.lines * 0.3)
-        elseif term.direction == "vertical" then
-          return math.floor(vim.o.columns * 0.8)
-        end
-        return 20
-      end,
-      open_mapping = [[<c-\>]],
-      shade_terminals = true,
-      start_in_insert = true,
-      direction = "float",
-      float_opts = {
-        border = "curved",
-        width = math.floor(vim.o.columns * 0.85),
-        height = math.floor(vim.o.lines * 0.85),
-      },
-    },
+    config = require('pluginsettings.toggleterm').setup,
   },
   -- LSP Plugins
   {
@@ -559,26 +541,34 @@ require("lazy").setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        -- Lua
         "stylua", -- Used to format Lua code
-        "shfmt",
+        "lua-language-server",
 
+        -- Shell
+        "shfmt",
+        "shellcheck",
+
+        -- Python
         "ruff",
         "mypy",
         "ruff-lsp",
         "pyright",
 
-        -- "r-languageserver",
+        -- Rust
+        "rust_analyzer",
 
+        -- LaTeX / Markdown
         "ltex-ls",
-
-        "stylua",
-        "lua-language-server",
-        "julia-lsp",
-        "json-lsp",
-        "cbfmt",
         "glow",
         "markdown-oxide",
         "marksman",
+
+        -- Misc.
+        -- "r-languageserver",
+        "julia-lsp",
+        "json-lsp",
+        "cbfmt",
       })
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
@@ -827,33 +817,6 @@ require("lazy").setup({
     --  - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --  - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --  This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
-  --
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
   ui = { icons = {} },
 })
