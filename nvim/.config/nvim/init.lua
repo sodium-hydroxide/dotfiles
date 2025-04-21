1c3d1115c24f41051b9a1d59ad9cdd8504b1d961
Noah Blair <43483259+sodium-hydroxide@users.noreply.github.com>
2025-04-21 11:32:57 -0600

    Clean up nvim tree configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -245,56 +245,7 @@ require("lazy").setup({
     version = "*",
     lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      local function on_attach(bufnr)
-        local api = require("nvim-tree.api")
-        local function opts(desc)
-          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-        end
-        api.config.mappings.default_on_attach(bufnr)
-      end
-
-      require("nvim-tree").setup({
-        sort_by = "case_sensitive",
-        view = {
-          width = 30,
-        },
-        renderer = {
-          group_empty = true,
-          icons = {
-            show = {
-              file = true,
-              folder = true,
-              folder_arrow = true,
-              git = true,
-              modified = true,
-            },
-            modified_placement = "after",
-            glyphs = {
-              modified = "●",
-            },
-          },
-          highlight_opened_files = "all",
-        },
-        filters = {
-          dotfiles = false,
-        },
-        git = {
-          enable = true,
-          ignore = false,
-        },
-        actions = {
-          open_file = {
-            quit_on_open = false,
-          },
-          change_dir = {
-            enable = true,
-            global = false,
-          },
-        },
-        on_attach = on_attach,
-      })
-    end,
+    config = require("pluginsettings.nvimtree").config,
   },
   {
     "akinsho/toggleterm.nvim",
